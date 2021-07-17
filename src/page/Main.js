c346e7d4b51e2c357d75a0e4ecba08f7ad9ed0b5
헹구 <85723134+hyemin085@users.noreply.github.com>
2021-07-17 22:20:24 +0900

    Update Main.js

--- src/page/Main.js
+++ src/page/Main.js
@@ -5,50 +5,56 @@ import styled from "styled-components";
 import "semantic-ui-css/semantic.min.css";
 import { Pagination } from "semantic-ui-react";
 
+import Header from "../components/Header";
+import Card from "../components/Card"
+
 const Main = () => {
   const PaginationExampleCompact = () => (
-    <Pagination
-      boundaryRange={0}
-      defaultActivePage={1}
-      ellipsisItem={null}
-      firstItem={null}
-      lastItem={null}
-      siblingRange={5}
-      totalPages={5}
-    />
+      <Pagination
+          boundaryRange={0}
+          defaultActivePage={1}
+          ellipsisItem={null}
+          firstItem={null}
+          lastItem={null}
+          siblingRange={5}
+          totalPages={5}
+      />
   );
 
   return (
-    <>
-      <Grid margin="2em 30em 0 10em">
-        <h1>입양하기</h1>
+      <>
+        <Header/>
+        <Grid margin="2em 30em 0 10em">
+          <div style={{border: "2px solid #699B97", width: "29px", marginBottom: "-20px"}}></div>
+          <h1>입양하기</h1>
 
-        <NoticeBox>
-          <YoutubeBox>유튜브</YoutubeBox>
-          <PhotoBox>공지 사진</PhotoBox>
-        </NoticeBox>
+          <NoticeBox>
+            <YoutubeBox>유튜브</YoutubeBox>
+            <PhotoBox>공지 사진</PhotoBox>
+          </NoticeBox>
 
-        <CardBox>
-          <Card />
-          <Card />
-          <Card />
-          <Card />
-          <Card />
-          <Card />
-        </CardBox>
+          <CardBox>
+            <Card />
+            <Card />
+            <Card />
+            <Card />
+            <Card />
+            <Card />
 
-        <PaginationContainer>
-          <PaginationExampleCompact />
-        </PaginationContainer>
-      </Grid>
-    </>
+          </CardBox>
+
+          <PaginationContainer>
+            <PaginationExampleCompact />
+          </PaginationContainer>
+        </Grid>
+      </>
   );
 };
 
 export default Main;
 
 const NoticeBox = styled.div`
-  width: 100%;
+  width: 150vh;
   height: 30vh;
   display: flex;
   margin: 2em auto;
@@ -58,7 +64,7 @@ const NoticeBox = styled.div`
 const YoutubeBox = styled.div`
   width: 50%;
   height: 30vh;
-  background-color: #c4c4c4;
+  background-color: #67BFB2;
 `;
 
 const PhotoBox = styled.div`
@@ -68,7 +74,7 @@ const PhotoBox = styled.div`
 `;
 
 const CardBox = styled.div`
-  width: 100%;
+  width: 150vh;
   height: auto;
   border: 1px solid black;
   box-sizing: border-box;
@@ -77,7 +83,7 @@ const CardBox = styled.div`
   gap: 4vh;
 `;
 
-const Card = styled.div`
+const Card1 = styled.div`
   height: 30vh;
   background-color: #e5cbbc;
   box-sizing: border-box;
@@ -89,4 +95,5 @@ const PaginationContainer = styled.div`
   display: flex;
   justify-content: center;
   align-items: center;
+  margin: auto;
 `;
